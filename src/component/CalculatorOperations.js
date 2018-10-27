d0bb4d2364dfa1b68e7a8f36cfeee9cacfcefb71
jamal-pb95 <jamal.pb95@gmail.com>
2018-10-27 23:42:18 +0600

    refactor

---
+++ src/component/CalculatorOperations.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CalculatorOperations = {
+  '/': (prevValue, nextValue) => prevValue / nextValue,
+  '*': (prevValue, nextValue) => prevValue * nextValue,
+  '+': (prevValue, nextValue) => prevValue + nextValue,
+  '-': (prevValue, nextValue) => prevValue - nextValue,
+  '=': (prevValue, nextValue) => nextValue
+}
+
+export default CalculatorOperations;
